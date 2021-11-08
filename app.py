ccdb59ded2ee6944d9fae57cc2f91367441a1515
Armonymon <71794386+atavakoulnia@users.noreply.github.com>
2021-11-08 12:23:25 -0800

    Simplified App

--- app.py
+++ app.py
@@ -8,21 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/characters', methods=['POST', 'GET'])
-def characters():
-    return render_template('characters.html')
-
 @app.route('/episodes', methods=['POST', 'GET'])
 def episodes():
     return render_template('episodes.html')
-
-@app.route('/music', methods=['POST', 'GET'])
-def music():
-    return render_template('music.html')
-
-@app.route('/world_map', methods=['POST', 'GET'])
-def world_map():
-    return render_template('world_map.html')
 
 
 if __name__ == "__main__":
