4d54b97fe2171e7db06136957bf965f4e76ec996
Armonymon <71794386+atavakoulnia@users.noreply.github.com>
2021-11-08 18:01:02 -0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, url_for, request, redirect
+import requests
 import os
+from IMDB_scraper.socket_client import receive, send
+from IMDB_scraper.IMDB_Scraper import imdb
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
+    
     return render_template('index.html')
+
 
 @app.route('/episodes', methods=['POST', 'GET'])
 def episodes():
+
     return render_template('episodes.html')
 
 
